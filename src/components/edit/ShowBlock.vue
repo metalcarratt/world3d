68d252436402fa9470c440c3aad53e0aeda67255
Caleb Garratt <metal.carratt@gmail.com>
2021-02-11 22:45:17 +1300

    show terrain brush when editing

---
+++ src/components/edit/ShowBlock.vue
@@ -0,0 +1,77 @@
+<template>
+    <div :id="id">
+    </div>
+</template>
+
+<script>
+import * as THREE from 'three';
+import Water from '@/models/water.js';
+import Grass from '@/models/grass.js';
+import Rock from '@/models/rock.js';
+import Tree from '@/models/tree.js';
+import brush from "@/components/edit/brush.js";
+
+export default {
+    props: ['id'],
+    data() {
+        return {
+            scene: null,
+            camera: null,
+            renderer: null,
+            block: null
+        }
+    },
+    computed: {
+        brush: () => brush.getBrush()
+    },
+    watch: {
+        brush() {
+            window.console.log("brush changed");
+            this.updateBlock();
+        }
+    },
+    mounted() {
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, 100 / 100, 0.1, 1000);
+        this.renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+        this.renderer.setSize(100, 100);
+            
+        this.$nextTick(function () {
+            const placeCanvas = document.getElementById(this.id);
+            placeCanvas.appendChild(this.renderer.domElement);
+        });
+
+        this.updateBlock();
+
+        this.camera.position.set(0.8, -0.8, 1.8);
+        this.camera.rotation.x = 0.49;
+        this.camera.rotation.y = 0.44;
+        this.camera.rotation.z = 0.68;
+
+        this.animate();
+    },
+    methods: {
+        animate() {
+            requestAnimationFrame(this.animate);
+            this.renderer.render(this.scene, this.camera);
+        },
+        updateBlock() {
+            window.console.log(`update brush: ${this.brush}`);
+            this.scene.remove(this.block);
+            if (this.brush === "" || this.brush === "water") {
+                this.block = Water.mesh();
+            } else if (this.brush === "grass") {
+                this.block = Grass.mesh();
+            } else if (this.brush === "rock") {
+                this.block = Rock.mesh();
+            } else if (this.brush === "tree") {
+                this.block = Tree.mesh();
+            }
+            
+            this.block.position.x = 0;
+            this.block.position.y = 0;
+            this.scene.add(this.block);
+        }
+    }
+}
+</script>
--- src/components/modes/ControlMode.vue
+++ src/components/modes/ControlMode.vue
@@ -2,12 +2,15 @@
     <div class="modes">
         <span :class="isWalking() ? 'selected' : ''" @click="changeModeToWalking">Walk</span>
         <span :class="isEditing() ? 'selected' : ''" @click="changeModeToEdit">Edit</span>
-        <select v-model="brush" v-if="isEditing()">
-            <option>water</option>
-            <option>grass</option>
-            <option>rock</option>
-            <option>tree</option>
-        </select>
+        <span v-if="isEditing()">
+            <select v-model="brush">
+                <option>water</option>
+                <option>grass</option>
+                <option>rock</option>
+                <option>tree</option>
+            </select>
+            <ShowBlock id="brush" :brush="brush"/>
+        </span>
     </div>
 </template>
 
@@ -16,8 +19,10 @@ import Modes from "./modes.js";
 import brush from "@/components/edit/brush.js";
 import selectorUtil from '@/components/board/selector.js';
 import playerUtil from '@/components/board/player.js';
+import ShowBlock from '@/components/edit/ShowBlock.vue';
 
 export default {
+    components: { ShowBlock },
     mounted() {
         window.addEventListener("keypress", this.keyboard);
     },
